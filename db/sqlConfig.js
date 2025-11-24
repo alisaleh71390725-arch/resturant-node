2f24b2938f3f2481cc58b506ab0ffcac2f8008c3
alisaleh71390725-arch <alisaleh71390725@gmail.com>
2025-11-24 14:20:26 +0200

    Create sqlConfig.js

---
+++ db/sqlConfig.js
@@ -0,0 +1,16 @@
+const sql = require('mssql');
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
+  port: 1433, 
+  options: {
+    encrypt: true, 
+
+    trustServerCertificate: true,
+  },
+};
+
+module.exports = { sql, config };
